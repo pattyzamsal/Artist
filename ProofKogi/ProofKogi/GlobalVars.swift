153c7f273469f7513b4c41f3242b656bebb0bca3
Patricia Zambrano <pzambrano@solsteace.com>
2017-03-16 18:56:46 -0400

    Créate global vars to pass values between views

---
+++ ProofKogi/ProofKogi/GlobalVars.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalVars.swift
+//  ProofKogi
+//
+//  Created by Devstn4 on 3/16/17.
+//  Copyright © 2017 Patricia Zambrano. All rights reserved.
+//
+
+import Foundation
+
+class SomeManager {
+    
+    var nameArtist = ""
+    
+    static let sharedInstance = SomeManager()
+
+}
