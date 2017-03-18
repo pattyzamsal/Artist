0db6a1e8de0601bb53449ff11e2c1eaba89b076a
pattyzamsal <pazs1991@gmail.com>
2017-03-18 15:29:18 -0400

    Add collection view to the view controller

---
+++ ProofKogi/ProofKogi/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  ProofKogi
+//
+//  Created by Patricia Zambrano on 3/18/17.
+//  Copyright © 2017 Patricia Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+}
