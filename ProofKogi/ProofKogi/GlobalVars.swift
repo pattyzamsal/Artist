8f57376f9c8e7913615164ebb373c5d8f87d3438
pattyzamsal <pazs1991@gmail.com>
2017-03-18 22:46:51 -0400

    Add global variables to save some things

--- ProofKogi/ProofKogi/GlobalVars.swift
+++ ProofKogi/ProofKogi/GlobalVars.swift
@@ -12,12 +12,19 @@ import UIKit
 
 class SomeManager {
     
+    // Search
     var idArtist = ""
     var nameArtist = ""
     var listURLImages = [String]()
     var listImages = [UIImage]()
     var popularity = 0
     var followers = 0
+    
+    // Albums
+    var listAlbums = [String]()
+    var listMarkets = [String]()
+    var listImagesAlbums = [UIImage]()
+    var listURLAlbums = [String]()
     
     static let sharedInstance = SomeManager()
 
